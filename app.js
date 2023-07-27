218b6dd375e4baff54001d01dc54125224edb308
Akhil <akhilk1910@gmail.com>
2023-07-27 20:11:50 +0530

    server file add

--- app.js
+++ app.js
@@ -247,3 +247,8 @@ let deletetasksFromList=(_listId)=>{
 //         res.send(tasks);
 //     })
 // });
+
+// const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+// const server = app.listen(port, function () {
+//     console.log('Server listening on port ' + port);
+// });
