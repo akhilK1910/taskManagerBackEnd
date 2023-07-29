8b3574e8a5351e19e33b2df1ca8e2936eee011a1
Akhil <akhilk1910@gmail.com>
2023-07-29 15:19:50 +0530

    redo

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://127.0.0.0:27017/TaskManager',{useNewUrlParser:true, useUnifiedTopology: true }).then(()=>{
+mongoose.connect('mongodb://0.0.0.0:27017/TaskManager',{useNewUrlParser:true, useUnifiedTopology: true }).then(()=>{
     console.log("Connected to MongoDB successfully");
 }).catch((e)=>{
     console.log("Error while attempting to connect to MongoDB");
